88aeda6442cedc4a2df7185c0fece72d48a9ebd5
Chloe Hwang <c.chloeh@gmail.com>
2017-03-09 13:38:09 -0500

    finalized set up so can now write in es6 and it works when npm installed

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import buttonStyle from '../styles/button'
 
 export default class Button extends React.Component {
 
   render () {
-    return (<button>HELddLO</button>)
+    return (<button style={buttonStyle}>HELddLO</button>)
   }
 
 }
